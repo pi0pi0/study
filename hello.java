bd6dcf1c40135dd88e563dc059a31a2cbec20756
pi0pi0 <ailblue@nate.com>
2017-06-26 17:02:12 +0900

    hello.java modified on local repository

--- hello.java
+++ hello.java
@@ -1,3 +1,6 @@
+// For command line git tutorial
+// Local repository modification
+
 i를 먼저 눌러야 read only에서 편집모드(insert)로 변경됩니다
 다쓰면  esc->콜론 wq사용 
 
@@ -5,9 +8,7 @@ git_add 로 local에 올린후 수정, git commit
 
 그다음에 git status로 확인한다.
 
-배고팡 
-
-
+//뀨
 
 ---------------------------------------------------------------------------------------------
 +++ 마스터 브랜치 말고
@@ -15,4 +16,5 @@ git_add 로 local에 올린후 수정, git commit
 
 ♥Tell my World!!!!!
 ☆ new ★
+
 
